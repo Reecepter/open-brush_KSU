76f4db706397c6337ee7ddea92c5897d703fe5e0
Lauren Rousell <laurenrousell@outlook.com>
2025-11-09 16:35:37 -0500

    Updated skybox script to include highlight

--- Assets/Lauren Skybox/SkyboxChanger.cs
+++ Assets/Lauren Skybox/SkyboxChanger.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class SkyboxChanger : MonoBehaviour
 {
     public Material[] skyboxes; // Assign skybox materials in Inspector
+    public Transform[] buttonTransforms;
+    public GameObject buttonHighlight;
+    private int currentIndex = 0;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeSkybox(currentIndex);
+        }
+        if (skyboxes == null || skyboxes.Length == 0)
+        {
+            RenderSettings.skybox = skyboxes[0];
+        }
+    }
 
     public void ChangeSkybox(int index)
     {
+        currentIndex = (currentIndex + 1) % skyboxes.Length;
         if (skyboxes == null || skyboxes.Length == 0)
         {
             Debug.LogWarning("Skybox list is empty or not assigned!");
@@ -18,6 +34,7 @@ public class SkyboxChanger : MonoBehaviour
         {
             Debug.Log("button clicked");
             RenderSettings.skybox = skyboxes[index];
+            buttonHighlight.transform.position = buttonTransforms[index].transform.position;
             DynamicGI.UpdateEnvironment();
         }
         else
